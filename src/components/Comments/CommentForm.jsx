63ab8e927fd72ea0cfa918c9ce95c88073ac8b48
Michelle Cheng <cheng.michelley@gmail.com>
2026-05-07 16:11:58 -0700

    Updated File Structure and Comments Components and Service

--- src/components/Comments/CommentForm.jsx
+++ src/components/Comments/CommentForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import { createComment } from "../services/commentService";
 
-const CommentForm = () => {
+const CommentForm = ({ postId, setComments }) => {
+
+  const [formData, setFormData] = useState({ body: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const newComment = await createComment(postId, formData);
+
+      setComments((prev) => [newComment, ...prev]);
+      setFormData({ body: "" });
+    } catch (err) {
+      console.log("Error creating comment:", err);
+    }
+  };
+
   return (
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="comment">Add a Comment:</label>
+      <textarea
+        name="body" 
+        id="comment"
+        value={formData.body}
+        onChange={(e) => setFormData({ body: e.target.value })}
+        required
+      />
+      <button type="submit">Submit Comment</button>
+    </form>
+  );
+};
 
-export default CommentForm
+export default CommentForm;
---
+++ src/components/Comments/CommentList.jsx
@@ -0,0 +1,22 @@
+import CommentItem from "./CommentItem";
+
+const CommentList = ({ comments, postId, user, setComments, postOwnerId }) => {
+  if (!comments || comments.length === 0) return <p>No comments yet.</p>;
+
+  return (
+    <ul>
+      {comments.map((comment) => (
+        <CommentItem
+          key={comment._id}
+          comment={comment}
+          postId={postId}
+          user={user}
+          setComments={setComments}
+          postOwnerId={postOwnerId}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default CommentList;
